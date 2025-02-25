f9cbc3d79b6fa1b23291e8bc367546211775abdb
denevanvenrooij <denevanvenrooij@gmail.com>
2025-02-25 10:15:55 +0100

    added median F0

--- extraction_functions_praat.py
+++ extraction_functions_praat.py
@@ -23,15 +23,15 @@ import numpy as np
 def PP_f0_mean(audio_file, f0_min=60, f0_max=300): ## the min/max are set based on standard for the human voice range
     sound = parselmouth.Sound(audio_file)
     
-    pitch = sound.to_pitch( ## the timestep is now 3.3ms as described by Murton (2023)
+    pitch = sound.to_pitch_cc( ## using to_pitch_cc() for extra sensitivity
         time_step=0.005, ## this can be adjusted to increase the window
         pitch_floor=f0_min, pitch_ceiling=f0_max)  
 
     f0 = pitch.selected_array['frequency']
-    f0 = f0[f0 > 0]  ## this removes unvoiced frames
+    f0 = f0[f0 > 0] ## this removes unvoiced frames
 
     if len(f0) == 0:
-        print("No frames were detected during F0 extraction, check audio file")
+        print("No frames were detected during PP_F0 extraction, check audio file")
         return None
 
     mean_f0 = np.mean(f0)
@@ -39,18 +39,37 @@ def PP_f0_mean(audio_file, f0_min=60, f0_max=300): ## the min/max are set based 
     return mean_f0 ## named PP_F0
 
 
-def PP_f0_sd(audio_file, f0_min, f0_max):
+def PP_f0_median(audio_file, f0_min=60, f0_max=300):
     sound = parselmouth.Sound(audio_file)
     
-    pitch = sound.to_pitch( ## the timestep is now 3.3ms as described by Murton (2023)
-        time_step=0.005, ## this can be adjusted to increase the window
+    pitch = sound.to_pitch_cc( 
+        time_step=0.005, 
         pitch_floor=f0_min, pitch_ceiling=f0_max)  
 
     f0 = pitch.selected_array['frequency']
-    f0 = f0[f0 > 0]  ## this removes unvoiced frames
+    f0 = f0[f0 > 0]
 
     if len(f0) == 0:
-        print("No frames were detected during F0_SD extraction, check audio file")
+        print("No frames were detected during PP_F02 extraction, check audio file")
+        return None
+
+    median_f0 = np.median(f0)
+    
+    return median_f0 ## named PP_F02
+
+
+def PP_f0_sd(audio_file, f0_min, f0_max):
+    sound = parselmouth.Sound(audio_file)
+    
+    pitch = sound.to_pitch_cc(
+        time_step=0.005,
+        pitch_floor=f0_min, pitch_ceiling=f0_max)  
+
+    f0 = pitch.selected_array['frequency']
+    f0 = f0[f0 > 0]
+
+    if len(f0) == 0:
+        print("No frames were detected during PP_F0_SD extraction, check audio file")
         return None
         
     f0_sd = np.std(f0)
@@ -61,18 +80,18 @@ def PP_f0_sd(audio_file, f0_min, f0_max):
 def PP_f0_mean_murton(audio_file, f0_min=60, f0_max=300): ## the min/max are set based on standard for the human voice range
     sound = parselmouth.Sound(audio_file)
     
-    pitch = sound.to_pitch( ## the timestep is now 3.3ms as described by Murton (2023)
-        time_step=0.0033, ## this can be adjusted to increase the window
+    pitch = sound.to_pitch_cc( 
+        time_step=0.0033, ## the timestep is now 3.3ms as described by Murton 2023/2017
         pitch_floor=f0_min, pitch_ceiling=f0_max)  
 
     f0 = pitch.selected_array['frequency']
-    f0 = f0[f0 > 0]  ## this removes unvoiced frames
+    f0 = f0[f0 > 0]
 
     if len(f0) == 0:
-        print("No frames were detected during F0 extraction, check audio file")
+        print("No frames were detected during PP_F0_M extraction, check audio file")
         return None
         
-    lower_bound, upper_bound = np.percentile(f0, [5, 95]) ## only includes 5-95 percentiles as described by Murton (2023)
+    lower_bound, upper_bound = np.percentile(f0, [5, 95]) ## only includes 5-95 percentiles as described by Murton 2023
     f0 = f0[(f0 >= lower_bound) & (f0 <= upper_bound)]
 
     mean_f0_murton = np.mean(f0)
@@ -80,21 +99,43 @@ def PP_f0_mean_murton(audio_file, f0_min=60, f0_max=300): ## the min/max are set
     return mean_f0_murton ## named PP_F0_M
 
 
-def PP_f0_sd_murton(audio_file, f0_min, f0_max):
+def PP_f0_median_murton(audio_file, f0_min=60, f0_max=300): ## the min/max are set based on standard for the human voice range
     sound = parselmouth.Sound(audio_file)
     
-    pitch = sound.to_pitch( ## the timestep is now 3.3ms as described by Murton (2023)
-        time_step=0.0033, ## this can be adjusted to increase the window
+    pitch = sound.to_pitch_cc( 
+        time_step=0.0033,
         pitch_floor=f0_min, pitch_ceiling=f0_max)  
 
     f0 = pitch.selected_array['frequency']
-    f0 = f0[f0 > 0]  ## this removes unvoiced frames
+    f0 = f0[f0 > 0]
 
     if len(f0) == 0:
-        print("No frames were detected during F0_SD extraction, check audio file")
+        print("No frames were detected during PP_F02_M extraction, check audio file")
         return None
         
-    lower_bound, upper_bound = np.percentile(f0, [5, 95]) ## only includes 5-95 percentiles as described by Murton (2023)
+    lower_bound, upper_bound = np.percentile(f0, [5, 95]) ## only includes 5-95 percentiles as described by Murton 2023
+    f0 = f0[(f0 >= lower_bound) & (f0 <= upper_bound)]
+
+    median_f0_murton = np.median(f0)
+    
+    return median_f0_murton ## named PP_F02_M
+
+
+def PP_f0_sd_murton(audio_file, f0_min, f0_max):
+    sound = parselmouth.Sound(audio_file)
+    
+    pitch = sound.to_pitch_cc(
+        time_step=0.0033,
+        pitch_floor=f0_min, pitch_ceiling=f0_max)  
+
+    f0 = pitch.selected_array['frequency']
+    f0 = f0[f0 > 0]
+
+    if len(f0) == 0:
+        print("No frames were detected during PP_F0_SD_M extraction, check audio file")
+        return None
+        
+    lower_bound, upper_bound = np.percentile(f0, [5, 95])
     f0 = f0[(f0 >= lower_bound) & (f0 <= upper_bound)]
 
     median_f0 = np.median(f0)
